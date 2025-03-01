0bf09736c87ed25e14d7cf248d6fd22338d63a63
Charles32-Dev <charles_fanautico@hotmail.com>
2025-03-01 20:00:30 -0300

    atualização de layout

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import Logo from '../../assets/logo.png'
+
+function Header() {
+
+
+    return (
+
+
+        <div>
+            <img src={Logo} alt='Logo-dev-movies' style={{ width: 500 }} />
+        </div>
+
+    )
+}
+
+
+export default Header
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,16 +3,20 @@ import { Route, Routes } from 'react-router-dom'
 import Home from '../containers/Home'
 import Movies from '../containers/Movies'
 import Series from '../containers/Series'
+import DefaultLayout from './DefaultLayout'
 
 
 function Router() {
 
     return (
         <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/Filmes' element={<Movies />} />
-            <Route path='/Series' element={<Series />} />
+            <Route element={<DefaultLayout />}>
+                <Route path='/' element={<Home />} />
+                <Route path='/Filmes' element={<Movies />} />
+                <Route path='/Series' element={<Series />} />
+            </Route>
         </Routes>
+
     )
 
 
